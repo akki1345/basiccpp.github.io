17c338783ab143305737b0bdce442d00d952b03b
Shuklavivek1807 <shuklavivek180797@gmail.com>
2022-07-13 14:41:55 +0530

    basic cpp

---
+++ basic.cpp
@@ -0,0 +1,91 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<endl<<"Enter a no in a:";
+    cin>>a;
+
+    cout<<endl<<"Enter a no in b:";
+    cin>>b;
+
+    c=a+b;
+
+    cout<<endl<<"ADDITION="<<c;
+    return 0;
+}
+
+
+
+// WAP for simple intrest.
+
+// int main(){
+//     int p,r,t,result;
+
+//     cout<<endl<<"Enter a principal value:";
+//     cin>>p;
+
+//     cout<<endl<<"Enter a time period in year:";
+//     cin>>t;
+    
+//     cout<<endl<<"Enter a rate of interest:";
+//     cin>>r;
+
+//     result = (p*r*t)/100;
+
+//     cout<<endl<<"Simple Interest="<<result;
+//     return 0;
+
+    
+// }
+
+
+
+// WAP for area and perimeter of a square.
+
+// int main(){
+//     int a,perimeter,area;
+//     cout<<endl<<"Enter a length:";
+//     cin>>a;
+    
+//     perimeter=4*a;
+//     area=a*a;
+
+//     cout<<endl<<"Perimeter of sqaure="<<perimeter;
+//     cout<<endl<<"Area of square="<<area;
+//     return 0;
+// }
+
+
+// WAP for area and perimeter of a rectangle.
+
+// int main(){
+//     int a,b,perimeter,area;
+//     cout<<endl<<"Enter a length:";
+//     cin>>a;
+
+//     cout<<endl<<"Enter a breadth:";
+//     cin>>b;
+    
+//     perimeter=2*a*b;
+//     area=a*b;
+
+//     cout<<endl<<"Perimeter of rectangle="<<perimeter;
+//     cout<<endl<<"Area of rectangle="<<area;
+//     return 0;
+// }
+
+
+
+// WAP to convert cenlsius to ferinhet
+
+// int main(){
+//     int c;
+//     cout<<endl<<"Enter a celsius value:";
+//     cin>>c;
+    
+//     float f = (9.0/5.0) * c + 32;
+
+//     cout<<endl<<"In farenheit="<<f;
+//     return 0;
+// }
